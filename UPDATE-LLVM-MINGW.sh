ff96887c7d272b1ca9ddf681e49b50c2c2880f38
F-Holm <moshishafcdodgecharger@gmail.com>
2025-07-23 15:16:31 +0000

    funciona download

--- UPDATE-LLVM-MINGW.sh
+++ UPDATE-LLVM-MINGW.sh
@@ -5,14 +5,12 @@ set -euo pipefail
 # Obtener JSON de GitHub release "nightly"
 json=$(curl -s https://api.github.com/repos/mstorsjo/llvm-mingw/releases/tags/nightly)
 
-echo "$json"
-
 # Usar script Python para extraer la URL
-#download_url=$(echo "$json" | ./get_latest_url.py)
+download_url=$(echo "$json" | ./GET-NAME-LLVM-MINGW-RELEASE.py)
 
 # Descargar el archivo
-#echo "Descargando: $download_url"
-#curl -LO "$download_url"
+echo "Descargando: $download_url"
+curl -LO "$download_url"
 
 # Limpiar caché de Python compilado
-#rm -rf ./__pycache__
+rm -rf ./__pycache__
