43b9e061f3006f0efe1ebd04c2638226e10903a7
F-Holm <101185761+F-Holm@users.noreply.github.com>
2025-07-23 01:58:05 -0300

    fix orden MySQL

--- INSTALL.sh
+++ INSTALL.sh
@@ -41,11 +41,11 @@ touch ~/.hushlogin
 sudo apt install -y curl wget git
 
 # MySQL
-sudo apt install -y mysql-server
-sudo mysql_secure_installation
 sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.32-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.32-1_all.deb
 sudo apt update
+sudo apt install -y mysql-server
+sudo mysql_secure_installation
 sudo apt install -y mysql-workbench-community
 sudo rm mysql-apt-config_0.8.32-1_all.deb
 
