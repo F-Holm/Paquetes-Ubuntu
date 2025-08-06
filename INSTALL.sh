39c8a2aa97b83867a0a64abddb542c34f59ad139
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-06 12:16:26 -0300

    Add: Docker

--- INSTALL.sh
+++ INSTALL.sh
@@ -83,6 +83,25 @@ sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo rm ./google-chrome-stable_current_amd64.deb
 
+# Docker (solo fuera de WSL)
+if [ -z "$WSL_INTEROP" ]; then
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
 # C++
 sudo apt install -y build-essential gcc g++ clang clang-format clang-tidy clangd clang-tools cmake make doxygen gdb graphviz ninja-build valgrind llvm lcov ccache gcc-i686-linux-gnu g++-i686-linux-gnu gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 sudo "$SCRIPT_DIR/CLANG-CROSS-COMPILING-WRAPPERS.sh"
