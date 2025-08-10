7e40543297f852aaec83e962f487caa9191893a6
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-10 20:03:54 -0300

    add usermod docker

--- INSTALL.sh
+++ INSTALL.sh
@@ -98,6 +98,7 @@ if [ -z "$WSL_INTEROP" ]; then
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
 
     # JetBrains
     sudo snap install intellij-idea-community --classic
