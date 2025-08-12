4997533e54273f3ad84a4b0d605463a5350ac999
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-12 02:14:03 +0000

    add symlinks to llvm-mingw

--- CLANG-CROSS-COMPILING-WRAPPERS.sh
+++ CLANG-CROSS-COMPILING-WRAPPERS.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-set -eux; \
-mkdir -p /usr/local/bin; \
-echo '#!/bin/sh' > /usr/local/bin/i686-linux-gnu-clang; \
-echo 'exec clang --target=i686-linux-gnu --sysroot=/usr/i686-linux-gnu "$@"' >> /usr/local/bin/i686-linux-gnu-clang; \
-chmod +x /usr/local/bin/i686-linux-gnu-clang; \
-\
-echo '#!/bin/sh' > /usr/local/bin/i686-linux-gnu-clang++; \
-echo 'exec clang++ --target=i686-linux-gnu --sysroot=/usr/i686-linux-gnu "$@"' >> /usr/local/bin/i686-linux-gnu-clang++; \
-chmod +x /usr/local/bin/i686-linux-gnu-clang++; \
-\
-echo '#!/bin/sh' > /usr/local/bin/arm-linux-gnueabihf-clang; \
-echo 'exec clang --target=arm-linux-gnueabihf --sysroot=/usr/arm-linux-gnueabihf "$@"' >> /usr/local/bin/arm-linux-gnueabihf-clang; \
-chmod +x /usr/local/bin/arm-linux-gnueabihf-clang; \
-\
-echo '#!/bin/sh' > /usr/local/bin/arm-linux-gnueabihf-clang++; \
-echo 'exec clang++ --target=arm-linux-gnueabihf --sysroot=/usr/arm-linux-gnueabihf "$@"' >> /usr/local/bin/arm-linux-gnueabihf-clang++; \
-chmod +x /usr/local/bin/arm-linux-gnueabihf-clang++; \
-\
-echo '#!/bin/sh' > /usr/local/bin/aarch64-linux-gnu-clang; \
-echo 'exec clang --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu "$@"' >> /usr/local/bin/aarch64-linux-gnu-clang; \
-chmod +x /usr/local/bin/aarch64-linux-gnu-clang; \
-\
-echo '#!/bin/sh' > /usr/local/bin/aarch64-linux-gnu-clang++; \
-echo 'exec clang++ --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu "$@"' >> /usr/local/bin/aarch64-linux-gnu-clang++; \
-chmod +x /usr/local/bin/aarch64-linux-gnu-clang++
+set -eux
+sudo mkdir -p /usr/local/bin
+
+# i686
+sudo echo '#!/bin/sh' > /usr/local/bin/i686-linux-gnu-clang
+sudo echo 'exec clang --target=i686-linux-gnu --sysroot=/usr/i686-linux-gnu "$@"' >> /usr/local/bin/i686-linux-gnu-clang
+sudo chmod +x /usr/local/bin/i686-linux-gnu-clang
+
+sudo echo '#!/bin/sh' > /usr/local/bin/i686-linux-gnu-clang++
+sudo echo 'exec clang++ --target=i686-linux-gnu --sysroot=/usr/i686-linux-gnu "$@"' >> /usr/local/bin/i686-linux-gnu-clang++
+sudo chmod +x /usr/local/bin/i686-linux-gnu-clang++
+
+# ARM 64-bit
+sudo echo '#!/bin/sh' > /usr/local/bin/aarch64-linux-gnu-clang
+sudo echo 'exec clang --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu "$@"' >> /usr/local/bin/aarch64-linux-gnu-clang
+sudo chmod +x /usr/local/bin/aarch64-linux-gnu-clang
+
+sudo echo '#!/bin/sh' > /usr/local/bin/aarch64-linux-gnu-clang++
+sudo echo 'exec clang++ --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu "$@"' >> /usr/local/bin/aarch64-linux-gnu-clang++
+sudo chmod +x /usr/local/bin/aarch64-linux-gnu-clang++
+
+# ARM 32-bit
+sudo echo '#!/bin/sh' > /usr/local/bin/arm-linux-gnueabihf-clang
+sudo echo 'exec clang --target=arm-linux-gnueabihf --sysroot=/usr/arm-linux-gnueabihf "$@"' >> /usr/local/bin/arm-linux-gnueabihf-clang
+sudo chmod +x /usr/local/bin/arm-linux-gnueabihf-clang
+
+sudo echo '#!/bin/sh' > /usr/local/bin/arm-linux-gnueabihf-clang++
+sudo echo 'exec clang++ --target=arm-linux-gnueabihf --sysroot=/usr/arm-linux-gnueabihf "$@"' >> /usr/local/bin/arm-linux-gnueabihf-clang++
+sudo chmod +x /usr/local/bin/arm-linux-gnueabihf-clang++
