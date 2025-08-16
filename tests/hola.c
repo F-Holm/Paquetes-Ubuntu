8ae016c18d4b49650dca01eeb09dc12d5e2b43bf
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-16 01:13:42 +0000

    add: tests & update: .gitignore

---
+++ tests/hola.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hola desde Linux Ubuntu x64\n");
+    return 0;
+}
---
+++ tests/hola.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hola desd Linux Ubuntu x64" << endl;
+    return 0;
+}
